a7af390539f344f4acc9ff37d1802d4a20605bc3
Gordonmajoni <gordonmajoni95@gmail.com>
2023-01-07 14:20:16 +0200

    new work

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int A = 90;
+        int B = 80;
+        int C = 70;
+        int D = 60;
+        
+    
+        
+        Console.Write("What is your grade?");
+        string grade = Console.ReadLine();
+        if(A  >=  B )
+        {
+            Console.WriteLine($"Congratulation your passed with{grade} percent");
+        }
+        else if(C >= D  )
+        {
+            Console.WriteLine($"You passed this time with{grade} percent grade");
+        }
+        else 
+        {
+            Console.WriteLine("Try again");
+        }
     }
 }
