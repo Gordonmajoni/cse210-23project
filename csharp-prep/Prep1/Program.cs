4a1edd9a8c6a169752df0f92425518497fa054b8
Gordonmajoni <gordonmajoni95@gmail.com>
2023-01-07 12:37:04 +0200

    corrections

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,11 +3,12 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
-        string name ="Gordon";
-        string lastname ="Majoni";
-        Console.WriteLine("What is your name?{name}");
-        Console.WriteLine("What is your lastname?{lastname}");
-        Console.WriteLine("Your name is ,{name} {lastname}");
+        string Name ="Gordon";
+        string Surname ="Majoni";
+        Console.WriteLine($"What is your name?{Name}");
+        Console.WriteLine($"What is your lastname?{Surname}");
+        Console.WriteLine($"Your name is ,{Name} {Surname}");
     }
 }
