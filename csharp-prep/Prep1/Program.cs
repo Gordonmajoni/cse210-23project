792f76beec457bf60aba13562c3b8c7670713070
Gordonmajoni <gordonmajoni95@gmail.com>
2023-01-07 12:27:29 +0200

    new task

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         string name ="Gordon";
-        string lastname ="Majoni"
+        string lastname ="Majoni";
         Console.WriteLine("What is your name?{name}");
         Console.WriteLine("What is your lastname?{lastname}");
         Console.WriteLine("Your name is ,{name} {lastname}");
