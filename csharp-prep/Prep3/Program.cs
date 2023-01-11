82f5e9f9aee680847135d42d2cc7017a25e38210
Gordonmajoni <gordonmajoni95@gmail.com>
2023-01-11 17:29:33 +0200

    try

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,15 +5,26 @@ class Program
     static void Main(string[] args)
     {
 
-
+//user input number.
         Console.Write("What is the magic number? "); 
         int magicNumber = int.Parse (Console.ReadLine());
-
+//user guess the number.
         Console.Write("What is your guess? ");
         int guessNumber = int.Parse (Console.ReadLine());
-
+//random number generator.
         Random randomGenerator = new Random();
         int Number = randomGenerator.Next(1, 11);
+
+        int guess = -1;
+        
+        while(guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+        }
+
+
 
 
     }
