655f28c497de70e8ed5a385aa4e851410fb40a5b
Gordonmajoni <gordonmajoni95@gmail.com>
2023-01-11 16:53:35 +0200

    correct

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,9 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number? ");        
-        Console.Write("What is your guess? ");
+        Console.Write("What is the magic number? "); 
         string number = Console.ReadLine();
+
+        Console.Write("What is your guess? ");
+        string input = Console.ReadLine();
         int num = int.Parse(number);
 
 
