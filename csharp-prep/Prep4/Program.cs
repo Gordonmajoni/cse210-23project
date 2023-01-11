09df3c4671e938f1f6e2a9c9b0d3acb829cc827d
Gordonmajoni <gordonmajoni95@gmail.com>
2023-01-11 22:00:27 +0200

    new

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,28 @@
 using System;
 
+using System.Collections.Generic;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //int i=0;
+
+        List<int> numbers = new List<int>();
+        numbers.Add(6);
+        numbers.Add(7);
+        numbers.Add(4);
+        numbers.Add(1);
+        numbers.Add(9);
+
+        
+
+        Console.WriteLine("The numbers in the list: ");
+
+    
+        {
+
+        }
+
     }
 }
