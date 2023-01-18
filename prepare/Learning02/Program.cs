58e028dde40a2fb2b0cc91d03320922a1c2ab569
Gordonmajoni <gordonmajoni95@gmail.com>
2023-01-18 18:08:43 +0200

    new

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,25 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        job1 =new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoftr";
-        job1._startYear ="2019";
-        job1._endYear ="2022";
+        Job job =new Job();
+        job._jobTitle = "Software Engineer";
+        job._company = "Microsoftr";
+        job._startYear ="2019";
+        job._endYear ="2022";
 
 
-        job2 =new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
-        job2._startYear ="2022";
-        job2._endYear ="2023";
+        job =new Job();
+        job._jobTitle = "Manager";
+        job._company = "Apple";
+        job._startYear ="2022";
+        job._endYear ="2023";
 
         Resume myResume = new Resume();
         myResume._name = "Allison Rose";
 
 
-        myResume._jobs.Add(job1);
-        myResume._jobs.Add(job2);
+        myResume._jobs.Add(job);
+        myResume._jobs.Add(job);
 
         myResume.Display();
 
